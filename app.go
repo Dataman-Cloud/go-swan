fc1f0f891a570adb1a4b903c8e13838b668c443f
Chu Xiangyang <xychu2008@gmail.com>
2017-03-08 14:42:15 +0800

    Add framework API support (#1)

--- app.go
+++ app.go
@@ -6,38 +6,6 @@ import (
 	"github.com/Dataman-Cloud/swan/src/types"
 )
 
-// Swan is the interface to the Swan API
-type Swan interface {
-	// -- APPLICATIONS ---
-	// create an application in swan
-	CreateApplication(version *types.Version) (*types.App, error)
-	// get a list of applications from swan
-	Applications(url.Values) ([]*types.App, error)
-	// delete an application in swan
-	DeleteApplication(appID string) error
-	// get an applications from swan
-	GetApplication(appID string) (*types.App, error)
-	// update an application in swan
-	UpdateApplication(appID string, version *types.Version) (*types.App, error)
-	// proceed the rolling update
-	ProceedUpdate(appID string, param *types.ProceedUpdateParam) error
-	// cancel the rolling update
-	CancelUpdate(appID string) error
-	// scale up the app
-	ScaleUp(appID string, param *types.ScaleUpParam) error
-	// scale down the app
-	ScaleDown(appID string, param *types.ScaleDownParam) error
-	// get versions of the app
-	GetAppVersions(appID string) ([]*types.Version, error)
-	// get the app version
-	GetAppVersion(appID, versionID string) (*types.Version, error)
-	// get the app task
-	GetAppTask(appID, taskIndex string) (*types.Task, error)
-
-	//-- SUBSCRIPTIONS--
-	AddEventsListener() (EventsChannel, error)
-}
-
 // CreateApplication creates a new application in Swan
 // version:the structure holding the application configuration
 func (r *swanClient) CreateApplication(version *types.Version) (*types.App, error) {
